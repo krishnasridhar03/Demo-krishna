c1d68914420cb22716ca68e908b122f50d9c5fde
Krishna Sridhar <krishna.lbck@gmail.com>
2025-02-26 15:38:37 +0530

    helo

--- src/test/java/demo/Democ2.java
+++ src/test/java/demo/Democ2.java
@@ -3,11 +3,12 @@ package demo;
 public class Democ2 {
 
 	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		float c = a+b;
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("c = " + c);
 		System.out.println("World");
-
-
 	}
 
 }
